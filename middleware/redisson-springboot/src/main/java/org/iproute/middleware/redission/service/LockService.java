c351a8914a908e112770db648c18a62e773bf948
zhuzhenjie <mimotronik@gmail.com>
2022-03-18 15:50:08 +0800

    fix: format

--- middleware/redisson-springboot/src/main/java/org/iproute/middleware/redission/service/LockService.java
+++ middleware/redisson-springboot/src/main/java/org/iproute/middleware/redission/service/LockService.java
@@ -6,8 +6,6 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * LockService
  *
