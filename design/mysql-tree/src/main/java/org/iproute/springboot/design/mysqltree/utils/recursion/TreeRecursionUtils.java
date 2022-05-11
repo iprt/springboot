1fe3e76ed58dbb40646a536011eb3a146b38fffa
zhuzhenjie <mimotronik@gmail.com>
2022-05-11 18:00:09 +0800

    fix: remove unused

--- design/mysql-tree/src/main/java/org/iproute/springboot/design/mysqltree/utils/recursion/TreeRecursionUtils.java
+++ design/mysql-tree/src/main/java/org/iproute/springboot/design/mysqltree/utils/recursion/TreeRecursionUtils.java
@@ -7,7 +7,6 @@ import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 /**
  * TreeRecursionUtils 树形结构递归处理封装
