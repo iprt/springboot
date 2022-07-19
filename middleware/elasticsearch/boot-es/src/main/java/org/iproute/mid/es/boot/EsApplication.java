1e93bfbf2a84df29b2072a918586e23cd9fbace5
zhuzhenjie <mimotronik@gmail.com>
2022-07-19 08:03:25 +0800

    feat: es actuator

--- middleware/elasticsearch/boot-es/src/main/java/org/iproute/mid/es/boot/EsApplication.java
+++ middleware/elasticsearch/boot-es/src/main/java/org/iproute/mid/es/boot/EsApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class EsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EsApplication.class, args);
     }
