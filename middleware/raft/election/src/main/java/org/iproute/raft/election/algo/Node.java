e70684ef8bf742cdd5b8a2df1468a877ee2ac69f
zhuzhenjie <zhuzj94@gmail.com>
2022-10-04 21:52:08 +0800

    fix: raft 缺少已经投票的判断

--- middleware/raft/election/src/main/java/org/iproute/raft/election/algo/Node.java
+++ middleware/raft/election/src/main/java/org/iproute/raft/election/algo/Node.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
