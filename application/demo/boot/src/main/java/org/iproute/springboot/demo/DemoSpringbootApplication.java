a149c1f75b76340322271ab32065932e531e271d
zhuzhenjie <zhuzj94@gmail.com>
2023-01-16 03:21:05 +0800

    feat: use my gradle's plugin in org.iproute.spring-boot-application fix: version strictly in product-platform fix: spring-boot-dependencies submodule's version

---
+++ application/demo/boot/src/main/java/org/iproute/springboot/demo/DemoSpringbootApplication.java
@@ -0,0 +1,18 @@
+package org.iproute.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DemoSpringbootApplication
+ *
+ * @author zhuzhenjie
+ * @since 2023/1/13
+ */
+@SpringBootApplication
+public class DemoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootApplication.class, args);
+    }
+}
