9dce5cc96aa812d6f6da3efff03b2678745fa38e
zhuzhenjie <zhuzj94@gmail.com>
2023-02-07 01:10:00 +0800

    feat: springboot integration
    add file writer integration
    1. xml config
    2. java config
    3. dsl config

---
+++ application/component/integration/integration-tcp-client/src/main/java/org/iproute/springboot/integration/controller/FileWriterController.java
@@ -0,0 +1,38 @@
+package org.iproute.springboot.integration.controller;
+
+import lombok.Data;
+import org.iproute.springboot.integration.filewriter.FileWriterGateway;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * FileWriterController
+ *
+ * @author zhuzhenjie
+ * @since 2023/2/6
+ */
+@RestController
+public class FileWriterController {
+
+    @Resource
+    private FileWriterGateway fileWriterGateway;
+
+    @PostMapping("/file/write")
+    public String writeToFile(@RequestBody FileWriterReq req) {
+        fileWriterGateway.writeToFile(
+                req.getFilename(),
+                req.getText()
+        );
+        return "ok";
+    }
+
+    @Data
+    public static class FileWriterReq {
+        private String filename;
+        private String text;
+    }
+
+}
--- application/component/integration/integration-tcp-client/src/main/java/org/iproute/springboot/integration/controller/HelloController.java
+++ application/component/integration/integration-tcp-client/src/main/java/org/iproute/springboot/integration/controller/HelloController.java
@@ -1,5 +1,6 @@
-package org.iproute.springboot.integration.tcpclient;
+package org.iproute.springboot.integration.controller;
 
+import org.iproute.springboot.integration.tcpclient.HelloGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
