1275dc4c4f1da433a2132eb10418233b61726d72
zhuzhenjie <zhuzj94@gmail.com>
2023-02-07 17:02:16 +0800

    feat: nacos client

--- application/component/alibaba/nacos-client/src/main/java/org/iproute/nacos/subservice/web/BeanNameController.java
+++ application/component/alibaba/nacos-client/src/main/java/org/iproute/nacos/subservice/web/BeanNameController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +20,6 @@ public class BeanNameController implements CommandLineRunner {
 
     @Value("${beanName}")
     private String beanName;
-
 
     @GetMapping("/getBeanName")
     public String getBeanName() {
