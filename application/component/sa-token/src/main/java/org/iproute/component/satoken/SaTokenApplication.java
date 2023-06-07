153b2acf8b353dd124f3bc35b8dbe0b7c2305a49
zhuzhenjie <mimotronik@gmail.com>
2023-06-07 19:23:03 +0800

    ft: sa-token demo

---
+++ application/component/sa-token/src/main/java/org/iproute/component/satoken/SaTokenApplication.java
@@ -0,0 +1,23 @@
+package org.iproute.component.satoken;
+
+import cn.dev33.satoken.SaManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SaTokenApplication
+ *
+ * @author zhuzhenjie
+ * @since 2023/6/5
+ */
+@SpringBootApplication
+@Slf4j
+public class SaTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenApplication.class, args);
+        log.info("启动成功：Sa-Token配置如下：{}", SaManager.getConfig());
+    }
+
+}
