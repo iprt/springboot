d769590c73160b490dca761f08a78bebe5d7e11d
zhuzhenjie <mimotronik@gmail.com>
2023-08-08 00:05:19 +0800

    fx

--- application/middleware/zookeeper/distribution-lock/src/main/java/org/iproute/middleware/zk/lock/config/Properties.java
+++ application/middleware/zookeeper/distribution-lock/src/main/java/org/iproute/middleware/zk/lock/config/Properties.java
@@ -1,7 +1,5 @@
 package org.iproute.middleware.zk.lock.config;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
