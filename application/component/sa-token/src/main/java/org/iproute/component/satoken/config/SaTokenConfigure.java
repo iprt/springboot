59263e3657925637029b72d0fd5da5c1c40b14c7
zhuzhenjie <mimotronik@gmail.com>
2023-08-15 01:22:22 +0800

    fx

---
+++ application/component/sa-token/src/main/java/org/iproute/component/satoken/config/SaTokenConfigure.java
@@ -0,0 +1,25 @@
+package org.iproute.component.satoken.config;
+
+import cn.dev33.satoken.fun.strategy.SaIsAnnotationPresentFunction;
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.stp.StpUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * SaTokenConfigure
+ *
+ * @author zhuzhenjie
+ * @since 2023/8/15
+ */
+@Configuration
+public class SaTokenConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/NoCookie/doLogin");
+    }
+}
