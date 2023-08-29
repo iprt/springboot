da9c56a1da7954eaec20b13d23387c6c748329dc
zhuzhenjie <mimotronik@gmail.com>
2023-08-29 23:18:35 +0800

    fix: raft request vote

--- application/middleware/raft/election/src/main/java/org/iproute/raft/election/algo/rpcs/RequestVote.java
+++ application/middleware/raft/election/src/main/java/org/iproute/raft/election/algo/rpcs/RequestVote.java
@@ -1,11 +1,32 @@
 package org.iproute.raft.election.algo.rpcs;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * 请求投票
  *
  * @author zhuzhenjie
  * @since 2023-08-27
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Data
 public class RequestVote {
+    // 候选人的任期号
+    private long term;
 
+    // 请求选票的候选人ID
+    private int candidateId;
+
+    // 候选人最后日志条目的索引值
+    private long lastLogIndex;
+
+    // 候选人最后日志条目的任期号
+    private long lastLongTerm;
 }
