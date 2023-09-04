802eec74031aadf51706232c44951e5de3bf7879
zhuzhenjie <mimotronik@gmail.com>
2023-09-04 23:35:37 +0800

    fix

---
+++ application/middleware/opentracing/src/main/java/org/iproute/mid/tracing/OpentracingApplication.java
@@ -0,0 +1,17 @@
+package org.iproute.mid.tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * OpentracingApplication
+ *
+ * @author zhuzhenjie
+ * @since 2023-09-01
+ */
+@SpringBootApplication
+public class OpentracingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpentracingApplication.class, args);
+    }
+}
---
+++ application/middleware/opentracing/src/test/java/org/iproute/mid/tracing/test/OpentracingApplicationTest.java
@@ -0,0 +1,10 @@
+package org.iproute.mid.tracing.test;
+
+/**
+ * OpentracingApplication
+ *
+ * @author zhuzhenjie
+ * @since 2023-09-01
+ */
+public class OpentracingApplicationTest {
+}
