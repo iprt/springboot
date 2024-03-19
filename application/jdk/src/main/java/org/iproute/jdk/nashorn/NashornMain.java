26c4cf25bf382aeffd78467173a2d9d678b44a54
zhenjie zhu <devops@kubectl.net>
2024-03-19 11:19:11 +0800

    Add NashornMain class and update Java version
    The NashornMain class has been added in the jdk package which makes use of Nashorn JavaScript engine. It logs the engine name and version, and evaluates a script block. Additionally, the source and target compatibility in the build logic has been updated to Java Version 11.

---
+++ application/jdk/src/main/java/org/iproute/jdk/nashorn/NashornMain.java
@@ -0,0 +1,45 @@
+package org.iproute.jdk.nashorn;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.List;
+
+/**
+ * NashornMain
+ *
+ * @author zhuzhenjie
+ */
+@Slf4j
+public class NashornMain {
+
+    public static final String ENGINE_NAME = "nashorn";
+
+    public static void main(String[] args) throws ScriptException {
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+
+        List<ScriptEngineFactory> engineFactories = scriptEngineManager.getEngineFactories();
+
+
+        engineFactories.forEach(ef -> {
+            String engineName = ef.getEngineName();
+            String engineVersion = ef.getEngineVersion();
+
+            log.info("engineName : {},engineVersion :{}", engineName, engineVersion);
+        });
+
+        ScriptEngine engine = scriptEngineManager.getEngineByName(ENGINE_NAME);
+
+        Object result = engine.eval(
+                "var greeting='hello world';" +
+                        "print(greeting);" +
+                        "greeting");
+
+        log.info("result: {}", result);
+    }
+
+
+}
