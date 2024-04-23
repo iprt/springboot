5b1219d7dd5b786ac16ef9218ab59257644694ef
zhenjie zhu <devops@kubectl.net>
2024-04-23 13:37:12 +0800

    Add NonNull annotation to DateConvertor
    NonNull annotation has been added to the convert method in the DateConvertor class. This change enforces a rule that the input string in this method should not be null, improving the method's robustness against null pointer exceptions.

--- application/core/bean-factory/src/main/java/org/iproute/core/factory/xml/convertor/DateConvertor.java
+++ application/core/bean-factory/src/main/java/org/iproute/core/factory/xml/convertor/DateConvertor.java
@@ -2,6 +2,7 @@ package org.iproute.core.factory.xml.convertor;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -17,7 +18,7 @@ public class DateConvertor implements Converter<String, Date> {
     private static final DateFormat FMT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Override
-    public Date convert(String str) {
+    public Date convert(@NonNull String str) {
         if (StringUtils.isBlank(str)) {
             return new Date();
         }
